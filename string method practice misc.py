fccda6f2005a4a7245ff2176e7dd65528695b146
imhim395 <yuvaangulati7689@gmail.com>
2026-03-19 22:02:49 -0400

    Initial Commit

---
+++ string method practice misc.py
@@ -0,0 +1,5 @@
+print("ilovemath".isalpha())#checking for letters(cant do any spaces)
+print("middleschoolsucks245243857436".isalnum())#checking for letters and numbers(also no spaces)
+print("67674141".isdecimal())#checking for numbers
+print("         ".isspace())#checking for spaces
+print("THEWORLDENDSTOMORROWLIVEEVERYDAYLIKEITSYOURLAST".istitle())#checking for titlecase(idk what that is)

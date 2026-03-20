4000854c6006a6fe2cab6922b022bd0a8ef28533
imhim395 <yuvaangulati7689@gmail.com>
2026-03-19 21:53:49 -0400

    Initial Commit

---
+++ string methods 1 pt 3.py
@@ -0,0 +1,2 @@
+print("ones success isnt determined by thier academic success".islower())
+print("ONES SUCCESS ISNT DETERMINES BY THIER ACADEMIC SUCCESS, BTW PRIVATE SCHOOL SUCKS(GO TO PUBLIC SCHOOL)".islower())

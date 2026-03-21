c4808b04fdc88df8f5a69bc92ede4e5a38e9fb3a
imhim395 <yuvaangulati7689@gmail.com>
2026-03-21 19:00:33 -0400

    Initial Commit

---
+++ .format pt 2.py
@@ -0,0 +1,6 @@
+name = input("What is the job apilcant's name?")
+degree = input("What did they major in at college?")
+job = input("What is their current occupation?")
+experience = input("How many years have they been working in their field?")
+
+print("{} majored in {}, works as a {}, and has {} years of experience.".format(name, degree, job, experience))
---
+++ .format.py
@@ -0,0 +1,6 @@
+name = input("What is the job apilcant's name?")
+degree = input("What did they major in at college?")
+job = input("What is their current occupation?")
+experience = input("How many years have they been working in their field?")
+
+print(name + " majored in " + degree + ", works as a " + job + ", and has " + experience + " years of experience.")#ts was mad annoying gng, we can use .format() to make it look better

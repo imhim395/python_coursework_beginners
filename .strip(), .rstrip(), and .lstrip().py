14c3d0bc5a5415347ab908ca1a1a413143e63575
imhim395 <yuvaangulati7689@gmail.com>
2026-03-21 18:25:55 -0400

    Initial Commit

---
+++ .strip(), .rstrip(), and .lstrip().py
@@ -0,0 +1,6 @@
+print("       yooooooooooooooooooooooooo".lstrip())#removes spaces from left side
+print("hello world                  ".rstrip())#remove spaces from right side
+print("67 is only tuff in michigan".strip("67")) #removes 67
+print("67 is only tuff in , michigan".strip(", michigan"))
+print("6767676767676767674167676767676767676767".strip("76"))
+print("blueblueyellowblue".strip("eulb"))

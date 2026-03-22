9204c2e3042caa09647f112bda57512a11653412
imhim395 <yuvaangulati7689@gmail.com>
2026-03-22 13:38:26 -0400

    Initial Commit

---
+++ .pop.py
@@ -0,0 +1,10 @@
+rappers = ["travis", "carti", "sofaygo", "liltecca"]
+end = rappers.pop() #pop is like pop the top, if this were sorted in a stack it would be like popping off the top of the stack, which in this case is lil tecca
+print(rappers)
+print(end)
+
+
+rappers = ["travis", "carti", "sofaygo", "liltecca"]
+end = rappers.pop(2)#2 is the index number, so it would print sofaygo
+print(rappers)
+print(end)

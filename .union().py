e269a5aa43b5e6ed61b6622a77ce950c0fa3583c
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 14:37:27 -0400

    Initial Commit

---
+++ .union().py
@@ -0,0 +1,9 @@
+fruits = {"apple", "orange", "banana", "tomato"}
+techstuff = {"mac", "code", "ai"}
+combo = techstuff.union(fruits)#combines two sets together
+print(combo)
+
+fruits = {"apple", "orange", "banana", "tomato"}
+techstuff = {"mac", "code", "ai"}
+combo = techstuff | fruits#combines two sets together as well
+print(combo)

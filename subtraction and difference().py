0f0f978d5d55f78e9f83a3764b2f48c17f087280
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 14:42:16 -0400

    Initial Commit

--- subtraction and difference().py
+++ subtraction and difference().py
@@ -1,9 +1,9 @@
 no = {1,2,3,4,5,6}
 yes = {0,1,2,3,4,5,6,7}
-subt = no - yes
+subt = no - yes# we can replace '-' with .difference because that also works
 print(subt)#doesnt give an output because their is nothing left u know what i mean
 
 no = {1,2,3,4,5,6}
 yes = {0,1,2,3,4,5,6,7}
-subt = yes - no
+subt = yes - no# we can replace '-' with .difference because that also works
 print(subt)#u guys know why ts works

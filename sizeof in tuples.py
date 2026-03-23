9e58628ddf08f56867d08a4107b3d1d177e69564
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 12:55:30 -0400

    Initial Commit

---
+++ sizeof in tuples.py
@@ -0,0 +1,4 @@
+tuple1 = (1, 3, 5)
+list1 = [1, 3, 5]
+print(tuple1.__sizeof__())#gives no. of bytes of storage being used, tuples are faster than lists because they have less memory usage
+print(list1.__sizeof__())

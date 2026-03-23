9a377db67cacda3acc97cd0d4abb00b721f6a85a
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 14:12:19 -0400

    Initial Commit

---
+++ .count().py
@@ -0,0 +1,4 @@
+repeat = (7, 3, 3, 3, 0, 0, 7, 0, 0)
+print(repeat.count(7))#repeats twice
+print(repeat.count(3))#repeats 3 times
+print(repeat.count(0))#repeats 4 times
---
+++ .index() in tuples.py
@@ -0,0 +1,3 @@
+ints = (1, 1, 7)
+print(ints.index(7))#finds out index no of 7
+print(ints.index(1))#finds index no of 1

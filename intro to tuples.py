a7f2a2bfc288eec1c3d72651e4a0ae735432a4a9
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 12:47:43 -0400

    Initial Commit

---
+++ intro to tuples.py
@@ -0,0 +1,3 @@
+tuple_1 = ("a", "b", "c", "d", "e")#in parentheses instead of square brackets
+tuple_2 = (2.718, False, [1, 2, 3])
+tuple_3 = (1, 1, 0, 0, 0)# u can have multiple instances repeating

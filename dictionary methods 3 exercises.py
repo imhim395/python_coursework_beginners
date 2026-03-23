7dd14ba5b84fb29c0a0dc6a00f971f9f2afa01ac
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 12:10:15 -0400

    Initial Commit

---
+++ dictionary methods 3 exercises.py
@@ -0,0 +1,8 @@
+internet_celebrities = {"DrDisrespect": "YouTube", "ZLaner": "Facebook", "Ninja": "Mixer"}
+another_one = {"shroud": "Twitch"}
+internet_celebrities.update(another_one)
+print(internet_celebrities)
+cityboi = internet_celebrities.copy()
+internet_celebrities.clear()
+print(internet_celebrities)
+print(cityboi)

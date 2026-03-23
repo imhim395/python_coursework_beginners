062ac14e7e904ae199f845b6dad412003b23d32f
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 10:33:29 -0400

    Initial Commit

---
+++ intro to dicitonaries.py
@@ -0,0 +1,3 @@
+boston_universities = {"boston universities": " northeastern" " harvard" " mit" " bu"}
+#start with curly brackets, then write a thing where you are showing a broad topic, then write stuff inside, called key values
+print(boston_universities["boston universities"])
